3ed945cbdf3f45a0577c0f4204fa965450b6a0c1
Zibenigais <110718712+Zibenigais@users.noreply.github.com>
2024-04-23 22:18:35 +0300

    Delete datubaze.py

--- datubaze.py
+++
@@ -1,6 +0,0 @@
-byte_string = b'Hello \xff World'  # Byte sequence with invalid UTF-8 character
-try:
-    decoded_string = byte_string.decode('utf-8')
-    print("Decoded string:", decoded_string)
-except UnicodeDecodeError as e:
-    print("Error decoding byte string:", e)
